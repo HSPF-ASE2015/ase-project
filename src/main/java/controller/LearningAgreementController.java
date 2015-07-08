f53eed4138a9f3f7dcd8dd6968d79cc8fac1d225
Jeroen <Jeroen@johngpaton.fritz.box>
2015-07-08 23:07:07 +0200

    added call to webservice to retrieve KURSE from parternerHochschule

--- src/main/java/controller/LearningAgreementController.java
+++ src/main/java/controller/LearningAgreementController.java
@@ -5,15 +5,21 @@
  */
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import fachklassen.Hochschule;
 import fachklassen.Kurs;
 import fachklassen.LearningAgreement;
 import fachklassen.Student;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import restfulServiceObjects.KursRESTServiceClient;
+import restfulServiceObjects.KursWS;
+import java.lang.reflect.Type;
 
 /**
  *
@@ -84,17 +90,39 @@ public class LearningAgreementController {
         }
     }
 
-        //Hier muss eigentlich der Webservice rein -- Als demo einfach mit DB-Abfrage von einer betimmten HS gemacht
-    public List<Kurs> getAlleAuslandsKurse() {
-        try {
-            Query query = em.createNamedQuery("getAlleInlandsKurse");
-            query.setParameter("HeimatHS", "SanDiego"); // Hier die ausgewählte AuslandsHS eintragen!!!!
-            return query.getResultList();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+       
+    public List<Kurs> getAlleAuslandsKurse(/* Hochschule partnerHochschule*/) {
+        Hochschule partnerHochschule = new Hochschule(); // wird später in die FUnktion als Parameter übergeben
+        
+        Type listType = new TypeToken<ArrayList<KursWS>>() {}.getType();
+        KursRESTServiceClient kursRESTServiceClient = new KursRESTServiceClient();
+        String kurseString = kursRESTServiceClient.findAll(String.class); 
+        List<KursWS> kurseWS = new Gson().fromJson(kurseString, listType);
+        
+        List<Kurs> kurseDB = new ArrayList<>();
+        
+        for(KursWS kursWS : kurseWS){
+            
+            if(kursWS.getErrorKurs() == null){
+                kurseDB.add(new Kurs(kursWS.getEcts(), kursWS.getName(), kursWS.getSprache(), partnerHochschule));
+            } else {
+                // TODO: error: exception?
+            }
         }
+       
+        kursRESTServiceClient.close();
+        
+        return kurseDB;
+        
+//        try {
+//            Query query = em.createNamedQuery("getAlleInlandsKurse");
+//            query.setParameter("HeimatHS", "SanDiego"); // Hier die ausgewählte AuslandsHS eintragen!!!!
+//            return query.getResultList();
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 
     public List<Hochschule> getPartnerHS() {
